4e1a04ad5d52a57e9996033c473aa3e74ca70e99
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-10-15 21:00:47 +0600

    all product fetched from db done

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -7,18 +7,33 @@ const createProduct = async (req: Request, res: Response) => {
     const productData = req.body;
     console.log(productData);
     const zodParsedData = productSchemaZod.parse(productData);
-    // const result = await ProductServices.createProductIntoDB(productData);
     const result = await ProductServices.createProductIntoDB(zodParsedData);
     res.status(200).json({
       success: true,
-      message: "product is created samu",
+      message: "product is created sam",
       data: result,
     });
   } catch (error) {
+    console.log("There is problem to create new product samiha");
+    console.log(error);
+  }
+};
+
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "All products retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log("There is error to get all products from db");
     console.log(error);
   }
 };
 
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
 };
